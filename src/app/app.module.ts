62fb1c70b47a09e0faf70e68fe25e864bbd59142
rahulchanglani <rahul.changlani@gmail.com>
2017-07-04 15:51:02 +0530

    step 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
@@ -10,8 +11,9 @@ import { AppComponent }  from './app.component';
     FormsModule // <-- import the FormsModule before binding with [(ngModel)]
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent, HeroDetailComponent ]
 })
 export class AppModule { }
